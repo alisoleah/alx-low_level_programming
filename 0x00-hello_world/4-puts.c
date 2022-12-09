304ae419c0fb57c2775329bf00397cd638e9b7b9
alisoleah <ali.soleah@gmail.com>
2022-12-09 10:39:30 -0800

    2 nd

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**                                                                                                                      
+ *
+ *  * main - Entry point                                                                                                  *
+ *                                                                                            
+ *
+ *    * Return: Always 0 (Success)    
+ *
+ *
+ * */                                                                                                                      
+int main(void)                                                                                                          
+
+{  
+char op1[51];
+
+
+strcpy(op1, "Programming is like building a multilingual puzzle");
+puts(op1);
+	    return (0);                                                                                                 
+}       
