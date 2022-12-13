92a883efaa1cae7402063d172d038f6923fc9bc4
alisoleah <ali.soleah@gmail.com>
2022-12-13 13:55:20 -0800

    1st

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - Entry point
+*
+* Description: 'print using puts'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/10alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - Entry point
+*
+* Description: 'print using puts'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char op1[52];
+strcpy(op1, "\"Programming is like building a multilingual puzzle");
+puts(op1);
+return (0);
+}
