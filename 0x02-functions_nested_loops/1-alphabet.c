ddab003970c4ce8b077f9f29d6ef80ec2fe86ad3
alisoleah <ali.soleah@gmail.com>
2022-12-13 16:59:58 -0800

    2nd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+* main - Entry point
+*
+* Description: 'print using puts'
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	int i;
+	for (i='a'; i<= 'z' ; i++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+
+}
