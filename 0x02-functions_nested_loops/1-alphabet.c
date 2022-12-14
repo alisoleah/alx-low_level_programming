6d780342c48990b41a301298dff20d86420090fd
alisoleah <ali.soleah@gmail.com>
2022-12-13 17:05:42 -0800

    2nd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,16 +4,16 @@
 /**
 * main - Entry point
 *
-* Description: 'print using puts'
+* print_alphabet to print lower case characters
 *
 * Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
 	int i;
-	for (i='a'; i<= 'z' ; i++)
+	for (i = 'a'; i <= 'z' ; i++)
 	{
-		_putchar(n);
+		_putchar(i);
 	}
 	_putchar('\n');
 
