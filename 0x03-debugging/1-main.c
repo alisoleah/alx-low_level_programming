7f8bfe72fbbed98353440a8a6746cf640dae2d98
alisoleah <ali.soleah@gmail.com>
2022-12-15 12:25:24 -0800

    3re nd

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,38 +1,21 @@
 #include <stdio.h>
-#include <string.h>
 #include "main.h"
 
 /**
-* times_table - to print 9 times table
+* main - main function
 *
-* Return: times table
+* Return: 0 (SUCCESS)
 */
-void times_table(void)
+int main(void)
 {
-int row, col, temp;
-for (row = 0; row <= 9; row++)
+int i;
+printf("Infinite loop incoming : (\n)");
+i = 0;
+while (i < 10)
 {
-_putchar('0');
-_putchar(',');
-_putchar(' ');
-for (col = 1; col <= 9; col++)
-{
-temp = (row * col);
-if ((temp / 10) > 0)
-{
-_putchar((temp / 10) + '0');
+putchar(i);
+i++;
 }
-else
-{
-_putchar(' ');
-}
-_putchar((temp % 10) + '0');
-if (col < 9)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
