47c463804515a3900402cc28fba453ff11a3d46a
alisoleah <ali.soleah@gmail.com>
2022-12-16 07:37:51 -0800

    3re nd

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isupper - Check UpperCase
+* @c: input
+* Return: 1 for upper letter or 0 for any else
+*/
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
