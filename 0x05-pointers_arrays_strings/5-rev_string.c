cbbc1bd40b9d99980f9d2c3f4f96985721c4b457
alisoleah <ali.soleah@gmail.com>
2022-12-19 11:04:54 -0800

    3re nd

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string -print string in reverse
+ *@s: pointer
+ *
+ *Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j, c;
+	char *t, temp;
+
+	t = s;
+
+	while (s[c] != '\0')
+		c++;
+	for (j = 1; j < c; j++)
+		t++;
+	for (i = 0; i < (c / 2); i++)
+	{
+		temp = s[i];
+		s[i] = *t;
+		*t = temp;
+		t--;
+	}
+
+	_putchar('\n');
+
+}
