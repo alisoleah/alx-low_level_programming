5ea381a8234e7421d14e2d04c09d4240912ac44e
alisoleah <ali.soleah@gmail.com>
2023-01-04 14:02:53 -0800

    3re nd

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,20 +7,20 @@
  */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt2(1, n));
+	return (_sqrt(1, n));
 }
 /**
- * _sqrt2 - find square
+ * _sqrt - find square
  * @a: integer
  * @b: integer
  * Return: value square
  */
-int _sqrt2(int a, int b)
+int _sqrt(int a, int b)
 {
 	if (a * a > b)
 		return (-1);
 	else if (a * a == b)
 		return (a);
 	else
-		return (_sqrt2(a += 1, b));
+		return (_sqrt(a += 1, b));
 }
