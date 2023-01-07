ea33d8aeb7e85cf874db2ef9663c7b743f39405d
alisoleah <ali.soleah@gmail.com>
2023-01-06 18:42:29 -0800

    3re nd

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - point
+ *@argc: string
+ *@argv: array
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, res = 1;
+
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < argc; i++)
+			res *= atoi(argv[i]);
+	}
+	printf("%d\n", res);
+	return (0);
+}
