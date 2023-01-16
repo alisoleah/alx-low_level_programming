83928bda29642525361d3fa2d7ad341a331c7ac8
alisoleah <ali.soleah@gmail.com>
2023-01-16 14:17:59 -0800

    3re nd

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,13 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Ali Galal
+ * Desc: Header file that defines a macro namedSIZE
+ * 	as an abbreviation for token 1024
+ */
+
 #define SIZE 1024
 
 #endif
