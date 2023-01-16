4d5dbd3b0ce51eafa004aaed1b1e87dfb77a4353
alisoleah <ali.soleah@gmail.com>
2023-01-16 14:22:17 -0800

    3re nd

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: PI_H
+ * Auth: Ali Galal
+ * Desc: Header file that defines a macro named PI
+ * 	as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
